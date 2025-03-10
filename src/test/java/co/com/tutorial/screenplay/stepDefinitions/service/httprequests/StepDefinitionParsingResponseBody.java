5dfe9eea4b6078dbbd5e6bd9bcfe1b0721657f58
Didier Ramirez <didier.ramirez@sofka.com.co>
2025-03-10 18:53:17 -0500

    amend-no-edit

---
+++ src/test/java/co/com/tutorial/screenplay/stepDefinitions/service/httprequests/StepDefinitionParsingResponseBody.java
@@ -0,0 +1,27 @@
+package co.com.tutorial.screenplay.stepDefinitions.service.httprequests;
+
+import io.cucumber.java.es.Cuando;
+import io.cucumber.java.es.Dado;
+import io.cucumber.java.es.Entonces;
+
+public class StepDefinitionParsingResponseBody {
+
+    //region Verificar el valor del segundo nombre de un Pokemon en estado salvaje
+    @Dado("que el {actor} cuenta con el recurso para encontrar un Pokemon en estado salvaje")
+    public void queElTesterCuentaConElRecursoParaEncontrarUnPokemonEnEstadoSalvaje() {
+
+    }
+    @Cuando("el {actor} hace la peticion para obtener el Pokemon en estado salvaje")
+    public void elTesterHaceLaPeticionParaObtenerElPokemonEnEstadoSalvaje() {
+
+    }
+    @Cuando("el {actor} valida el segundo nombre de un Pokemon en estado Salvaje")
+    public void elTesterValidaElSegundoNombreDeUnPokemonEnEstadoSalvaje() {
+
+    }
+    @Entonces("el {actor} valida que el valor del segundo nombre del Pokemon en estado salvaje coincide con el valor esperado")
+    public void elTesterValidaQueElValorDelSegundoNombreDelPokemonEnEstadoSalvajeCoincideConElValorEsperado() {
+
+    }
+    //endregion Verificar el valor del segundo nombre de un Pokemon en estado salvaje
+}
