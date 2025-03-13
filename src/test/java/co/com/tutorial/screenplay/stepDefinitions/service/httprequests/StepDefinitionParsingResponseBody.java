6cd3db5f89339cdfe6b0bca530863aeae9c0cc85
Didier Ramirez <didier.ramirez@sofka.com.co>
2025-03-12 23:28:21 -0500

    amend-no-edit

--- src/test/java/co/com/tutorial/screenplay/stepDefinitions/service/httprequests/StepDefinitionParsingResponseBody.java
+++ src/test/java/co/com/tutorial/screenplay/stepDefinitions/service/httprequests/StepDefinitionParsingResponseBody.java
@@ -1,18 +1,26 @@
 package co.com.tutorial.screenplay.stepDefinitions.service.httprequests;
 
+import co.com.tutorial.screenplay.exceptions.TestFailure;
 import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.es.Entonces;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
 import net.serenitybdd.screenplay.rest.interactions.Get;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static net.serenitybdd.screenplay.rest.questions.ResponseConsequence.seeThatResponse;
 import static org.hamcrest.Matchers.equalTo;
 
 public class StepDefinitionParsingResponseBody {
+
+    Logger LOGGER = LoggerFactory.getLogger(StepDefinitionParsingResponseBody.class);
 
     //region Verificar el valor del segundo nombre de un Pokemon en estado salvaje
     @Dado("que el {actor} cuenta con el recurso para encontrar un Pokemon en estado salvaje")
@@ -31,16 +39,30 @@ public class StepDefinitionParsingResponseBody {
 
     @Entonces("el {actor} observa que el valor del segundo nombre del Pokemon en estado salvaje coincide con el valor esperado")
     public void elTesterObservaQueElValorDelSegundoNombreDelPokemonEnEstadoSalvajeCoincideConElValorEsperado(Actor tester) {
-        String bodyResponse = SerenityRest.lastResponse().getBody().asString();
         tester.should(seeThatResponse(
-                        "Se han obtenido las cookies del sitio web",
+                        "Los datos obtenidos son correctos",
                         validatableResponse -> validatableResponse
                                 .statusCode(200)
                                 .header("Content-Type", "application/json; charset=utf-8").and()
                                 .body("names[1].language.name", equalTo("de"))
-                                .log().all()
-                )
+                                .log().all())
+                .orComplainWith(TestFailure.class, "Error en las aserciones")
         );
     }
     //endregion Verificar el valor del segundo nombre de un Pokemon en estado salvaje
+
+    //region Verificar la respuesta a una peticion con Response de rest-azure
+    @Entonces("el tester observa que los valores esperados son iguales a los del Response de rest-azure")
+    public void elTesterObservaQueLosValoresEsperadosSonIgualesALosDelResponseDeRestAzure() {
+        Response response = SerenityRest.lastResponse();
+
+        int statusCode = response.getStatusCode();
+        MatcherAssert.assertThat("El codigo de estado es 200", statusCode, Matchers.is(200));
+        LOGGER.info("Status code was = {}", statusCode);
+
+        String languageName = response.jsonPath().getString("names[0].language.name");
+        MatcherAssert.assertThat("El lenguaje es frances", languageName, Matchers.is("fr"));
+        LOGGER.info("The language was = {}", languageName);
+    }
+    //endregion Verificar la respuesta a una peticion con Response de rest-azure
 }
--- src/test/resources/features/service/httprequests/ParsingResponseBody.feature
+++ src/test/resources/features/service/httprequests/ParsingResponseBody.feature
@@ -2,7 +2,12 @@
 @FeatureName:ParsingResponseBody
 Característica: : Validación del campo "status" en la API de tareas
 
-  Escenario: Verificar el valor del segundo nombre de un Pokemon en estado salvaje
+  Escenario: Verificar la respuesta correcta de una peticion desde un valor en el body
     Dado que el tester cuenta con el recurso para encontrar un Pokemon en estado salvaje
     Cuando el tester hace la peticion para obtener el Pokemon en estado salvaje
     Entonces el tester observa que el valor del segundo nombre del Pokemon en estado salvaje coincide con el valor esperado
+
+  Escenario: Verificar la respuesta a una peticion con Response de rest-azure
+    Dado que el tester cuenta con el recurso para encontrar un Pokemon en estado salvaje
+    Cuando el tester hace la peticion para obtener el Pokemon en estado salvaje
+    Entonces el tester observa que los valores esperados son iguales a los del Response de rest-azure
