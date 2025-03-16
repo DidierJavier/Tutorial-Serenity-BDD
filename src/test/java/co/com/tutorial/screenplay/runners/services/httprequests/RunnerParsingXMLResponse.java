776565b8373e690872ed1a8c5799fe0129ff8d87
Didier Ramirez <didier.ramirez@sofka.com.co>
2025-03-15 19:28:23 -0500

    amend-no-edit

---
+++ src/test/java/co/com/tutorial/screenplay/runners/services/httprequests/RunnerParsingXMLResponse.java
@@ -0,0 +1,17 @@
+package co.com.tutorial.screenplay.runners.services.httprequests;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/service/httprequests/ParsingXMLResponse.feature",
+        glue = {"co.com.tutorial.screenplay.stepDefinitions",
+                "co.com.tutorial.screenplay.hooks"},
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        plugin = {"pretty"},
+        tags = "@FeatureName:ParsingXMLResponse"
+)
+public class RunnerParsingXMLResponse {
+}
---
+++ src/test/java/co/com/tutorial/screenplay/stepDefinitions/service/httprequests/StepDefinitionParsingXMLResponse.java
@@ -0,0 +1,77 @@
+package co.com.tutorial.screenplay.stepDefinitions.service.httprequests;
+
+import io.cucumber.java.es.Cuando;
+import io.cucumber.java.es.Dado;
+import io.cucumber.java.es.Entonces;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.path.xml.config.XmlPathConfig;
+import net.serenitybdd.rest.SerenityRest;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
+import net.serenitybdd.screenplay.rest.interactions.Get;
+import org.hamcrest.MatcherAssert;
+
+
+// Important!
+import static net.serenitybdd.screenplay.rest.questions.ResponseConsequence.seeThatResponse;
+import static org.hamcrest.Matchers.*;
+
+public class StepDefinitionParsingXMLResponse {
+
+    //Verificar la respuesta correcta de una peticion desde un valor en el body XML
+    @Dado("que el {actor} cuenta con el recurso para obtener informacion XML")
+    public void queElTesterCuentaConElRecursoParaObtenerInformacionXML(Actor tester) {
+        tester.whoCan(CallAnApi.at("https://www.ecb.europa.eu"));
+    }
+
+    @Cuando("el {actor} hace la peticion para obtener la informacion en formato XML")
+    public void elTesterHaceLaPeticionParaObtenerLaInformacionEnFormatoXML(Actor tester) {
+        Get.resource("/stats/eurofxref/eurofxref-daily.xml")
+                .with(requestSpecification -> requestSpecification
+                        .contentType("application/xml")
+                        .log().all())
+                .performAs(tester);
+    }
+
+    @Entonces("el {actor} observa que la respuesta XML es correcta")
+    public void elTesterObservaQueLaRespuestaXMLEsCorrecta(Actor tester) {
+        // Obtener la respuesta XML
+        String xmlResponse = SerenityRest.lastResponse().getBody().asString();
+
+        // Crear XmlPath con namespaces
+        XmlPath xmlPath = new XmlPath(xmlResponse)
+                .using(new XmlPathConfig()
+                        .declaredNamespace("gesmes", "http://www.gesmes.org/xml/2002-08-01")
+                        .declaredNamespace("eurofxref",
+                                "http://www.ecb.int/vocabulary/2002-08-01/eurofxref"));
+
+        // Validar la respuesta
+        tester.should(seeThatResponse("Se han obtenido los datos esperados del usuario",
+                response -> response
+                        .statusCode(200)
+                        .header("Content-Type", containsString("text/xml"))
+                        .log().all()
+        ));
+
+        // Validar los valores XML con XmlPath
+        MatcherAssert.assertThat(xmlPath.getString("gesmes:Envelope.gesmes:subject"),
+                equalTo("Reference rates"));
+        MatcherAssert.assertThat(xmlPath.getString("gesmes:Envelope.gesmes:Sender.gesmes:name"),
+                equalTo("European Central Bank"));
+        MatcherAssert.assertThat(xmlPath.getString("gesmes:Envelope.eurofxref:Cube.eurofxref:Cube.@time"),
+                equalTo("2025-03-14"));
+        MatcherAssert.assertThat(xmlPath
+                        .getString("gesmes:Envelope.eurofxref:Cube.eurofxref:Cube.eurofxref:Cube[1].@currency"),
+                equalTo("JPY"));
+        MatcherAssert.assertThat(xmlPath
+                        .getString("gesmes:Envelope.eurofxref:Cube.eurofxref:Cube.eurofxref:Cube[1].@rate"),
+                equalTo("161.88"));
+        MatcherAssert.assertThat(xmlPath
+                        .getString("gesmes:Envelope.eurofxref:Cube.eurofxref:Cube.eurofxref:Cube[10].@currency"),
+                equalTo("CHF"));
+        MatcherAssert.assertThat(xmlPath
+                        .getString("gesmes:Envelope.eurofxref:Cube.eurofxref:Cube.eurofxref:Cube[10].@rate"),
+                equalTo("0.9641"));
+    }
+    //Verificar la respuesta correcta de una peticion desde un valor en el body XML
+}
